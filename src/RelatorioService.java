4a0031e6b7ad3ac976be09ccd230e5473db7617e
Estella1 <estella.santos.moreira@gmail.com>
2025-02-07 22:23:33 -0300

    relatorio extends ControleFinanceiro

--- src/RelatorioService.java
+++ src/RelatorioService.java
@@ -1,5 +1,8 @@
 // Classe responsável pela geração de relatórios
-public class RelatorioService {
+public class RelatorioService extends ControleFinanceiro {
+    //calcular gastos com cada metodo de pagamento
+    //calacular gastos com cada categoria
+    //Gerar relatorio com gastos gerais
     public void gerarRelatorioSaldo(Usuario usuario) {
         // Implementação
     }
--- src/Relatorios.java
+++
@@ -1,6 +0,0 @@
-public class Relatorios extends ControleFinanceiro{
-    //resumo das transações
-    //Calcular quanto gasto em cada método de pagamento
-    //Calcular quanto gasto em cada categoria
-
-}
