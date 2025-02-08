993dfa23cc384779fd85d930888b8f86de88722d
Estella1 <estella.santos.moreira@gmail.com>
2025-02-07 21:35:09 -0300

    criei classe relatorios

---
+++ src/Relatorios.java
@@ -0,0 +1,6 @@
+public class Relatorios extends ControleFinanceiro{
+    //resumo das transações
+    //Calcular quanto gasto em cada método de pagamento
+    //Calcular quanto gasto em cada categoria
+
+}
